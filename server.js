f7991287fb0e9167e26ecdba9837eeffa1c315ae
Dhruv Garg <dhrgarg1@publicisgroupe.net>
2023-10-17 02:47:51 +0530

    added error tracking

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.static('public'));
 app.use(express.text());
 
 app.get('/status', (req, res) => {
+    console.log('GET /status');
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
@@ -15,23 +16,29 @@ app.get('/status', (req, res) => {
 
     // Send the current status immediately
     res.write(`data: ${status}\n\n`);
+    console.log(`Sent status: ${status}`);
 
     // Add this client to the clients array
     clients.push(res);
+    console.log(`Added client. Total clients: ${clients.length}`);
 
     // Remove this client from the clients array when the connection is closed
     req.on('close', () => {
         clients = clients.filter(client => client !== res);
+        console.log(`Removed client. Total clients: ${clients.length}`);
     });
 });
 
 app.post('/status', (req, res) => {
+    console.log('POST /status');
     status = req.body;
+    console.log(`Received status: ${status}`);
 
     // Send the new status to all connected clients
-    clients.forEach(client =>
-        client.write(`data: ${status}\n\n`)
-    );
+    clients.forEach(client => {
+        client.write(`data: ${status}\n\n`);
+        console.log(`Sent status to client: ${status}`);
+    });
 
     res.end();
 });
