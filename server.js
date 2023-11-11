feba7113e2f13ef7db31f8a6a710a707f9925a55
Dhruv Garg <dhrgarg1@publicisgroupe.net>
2023-11-11 19:43:07 +0530

    env2

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const admin = require('firebase-admin');
 const app = express();
 var bodyParser = require('body-parser')
+require('dotenv').config()
 
 app.use(express.static('public'));
 app.use(express.text());
